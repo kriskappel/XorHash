abd0223d1932cd78b171c741b1250a7748c613f1
karinepestana <karine_pestana_34@hotmail.com>
2016-10-19 19:50:12 -0200

    continuando a inserção

--- hashTable.cpp
+++ hashTable.cpp
@@ -13,20 +13,35 @@ struct nod{
 typedef struct nod node;
 
 node *createHash(int tam);//Cria hash inicial
-void insertToHash(node *hash, string content, int colisao);//Insere na hash
+void insertToHash(node *hash, string content, int colisao, int flag, int tam);//Insere na hash
+void insereEncadeamento(node *hash, node *novo, int key);
+void insereLinear(node *hash, node *novo, int key);
+void insereQuadratica(node *hash, node *novo, int key);
+void insereHashDuplo(node *hash, node *novo, int key);
 int stringXor(string content);//Calcula função xor
 
 int main()
 {
-	int tam= 500;
-	int colisao;
+	int tam= 500, qtd=0;
+	int colisao, flag=0;
+	double fator;
 	node *hashTable = createHash(tam);
 	string tag, content;
-	cin>>tag>>content;
+	cin>>tag>>content;		
+	
 	if(tag=="INSERT")
 	{
-		cout<<"insert"<<endl;
-		insertToHash(hashTable, content, colisao);
+		cout<<"insert"<<endl;		
+		insertToHash(hashTable, content, colisao, flag, tam);
+		qtd++; //adiciona mais um na quantidade
+		if(qtd == 0)	
+			fator=0;	
+		else
+		{
+			fator = (double)qtd/tam; //calcula o fator de carga
+			if(fator >= 0.75)
+				flag=1; //ocorre o rehash
+		}
 	}
 	else if(tag=="DELETE")
 		cout<<"delete"<<endl;
@@ -53,10 +68,10 @@ node *createHash(int tam)
 	return aux;
 }	
 
-void insertToHash(node *hash, string content, int colisao)
+void insertToHash(node *hash, string content, int colisao, int tam)
 {
 	cout<<content<<endl;
-
+	int key, pos;
 	node *newNode;
 	newNode=new node;
 	if(newNode == NULL){
@@ -70,7 +85,24 @@ void insertToHash(node *hash, string content, int colisao)
 		//newNode->c=content;//.copy(content, content.length(), 0);
 		newNode->c=content;
 		//cout<<newNode->c<< "só pra ter certeza"<<endl;	
-		int key=stringXor(content);
+		key=stringXor(content); //retorna onde deve ser inserido na hash
+		pos = key%tam; //faz a função k mod m
+		//sem colisão, insere normal da hash
+		if(hash[pos] == NULL)
+			hash[pos] = newNode;
+		else{
+			//ocorreu a colisão, tratara ela com o modo adequado
+			switch(colisao){
+				case 0:
+				insereEncadeamento(hash,newNode, key);			
+				case 1:
+				insereLinear(hash,newNode, key);
+				case 2:
+				insereQuadratica(hash,newNode, key);
+				case 3:
+				insereHashDuplo(hash,newNode, key);
+			}
+		}
 	}
 }
 
