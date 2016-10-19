6961ad59b0c6ca20db5a9cef17a6d21c4f8d3cc9
kriskappel <kristofer.kappel@gmail.com>
2016-10-19 16:41:50 -0200

    starting creating insertion to hash

--- hashTable.cpp
+++ hashTable.cpp
@@ -1,28 +1,47 @@
 #include <stdio.h>
 #include <string>
+#include <cstring>
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
 
-struct node
-{
-	long long int k;
+struct nod{
 	string c;
+	struct nod *prox;
 };
-typedef node node;
+typedef struct nod node;
 
 node *createHash(int tam);//Cria hash inicial
-node *insertToHash(string content);//Insere na hash
-int hashFunction(string content);//Calcula função xor
+void insertToHash(node *hash, string content, int colisao);//Insere na hash
+int stringXor(string content);//Calcula função xor
 
 int main()
 {
 	int tam= 500;
+	int colisao;
 	node *hashTable = createHash(tam);
-	string teste= "umaString";
-	node *newNode;
-	newNode=insertToHash(teste);
+	string tag, content;
+	cin>>tag>>content;
+	if(tag=="INSERT")
+	{
+		cout<<"insert"<<endl;
+		insertToHash(hashTable, content, colisao);
+	}
+	else if(tag=="DELETE")
+		cout<<"delete"<<endl;
+	else if(tag=="GET")
+		cout<<"get"<<endl;
+	else
+		cout<<"ERROR, UNDEFINED TAG"<<endl;
+
+	cout<<content<<endl;
+	
+	//string teste= "umaString";
+	//cout<<teste<<endl;
+	//node *nNode;
+	//insertToHash(teste);
+	//cout<<"string"<<endl;
 
 }
 
@@ -32,26 +51,37 @@ node *createHash(int tam)
 	aux=(node*)malloc(sizeof(node)*tam);
 	
 	return aux;
-}
+}	
 
-node *insertToHash(string content)
+void insertToHash(node *hash, string content, int colisao)
 {
-	node *newNode; //=(node*)malloc(sizeof(node));
-	
-	newNode->c=content;
-	int key=hashFunction(content);
-	newNode->k=key;
-	return newNode;
+	cout<<content<<endl;
+
+	node *newNode;
+	newNode=new node;
+	if(newNode == NULL){
+		cout<<"nulo";
+	}
+	else
+	{
+		//cout<<"aqui estamos"<<endl;
+		//cout<<(*newNode).c<<endl;
+		//strcpy(newNode->c, content.c_str());
+		//newNode->c=content;//.copy(content, content.length(), 0);
+		newNode->c=content;
+		//cout<<newNode->c<< "só pra ter certeza"<<endl;	
+		int key=stringXor(content);
+	}
 }
 
-int hashFunction(string content)
+int stringXor(string content)
 {
     unsigned h = 0;
 
     for (int i = 0; i < content.length(); i++)
     {
         h ^= content[i];
-        cout<< "iteração "<<i<<" "<<h<<endl;
+        //cout<< "iteração "<<i<<" "<<h<<endl;
     }
     cout<< h<<endl;
     return h;
