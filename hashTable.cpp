9576d390031458007ad913a2d12eb9c7ccef86c7
kriskappel <kristofer.kappel@gmail.com>
2017-02-16 19:16:29 -0200

    almost finished, needs to work w files and also needs to calculate the time

--- hashTable.cpp
+++ hashTable.cpp
@@ -46,8 +46,10 @@ int hash1(int key, int tam);//calcula h1(x)
 int hash2(int key, int tam);//calcula h2(x) no caso de double hashing
 int selectTreatment(char **argv);
 
+string checkString(string content);
+
 int main(int argc, char **argv) {
-    int tam = 5, qtd = 0; //tamanho incial da hash = 500
+    int tam = 500, qtd = 0; //tamanho incial da hash = 500
     int colisao, flag = 0;
     bool status;
     double fator;
@@ -75,14 +77,8 @@ int main(int argc, char **argv) {
 
     //Le a tag, referente ao tipo de operaçao, insert, get ou delete
     //Le o conteudo que quer se por na hash
-    cin >> tag >> content;
-    while(content.length()>99)
-    {
-        cout<<"ERROR"<<endl;
-        cin>>content;
-    }
-
-
+	cin >> tag >> content;
+	content=checkString(content);
     //cout<<argc;
 
     //cout<<*argv[1];
@@ -91,8 +87,18 @@ int main(int argc, char **argv) {
     while (tag != "0") // se a tag for 0 ele para a execução
     {
         if (tag == "INSERT") {
-            cout << "INSERT" << endl;//só pra fins de teste
+            cout << "INSERT " << content << " ";//só pra fins de teste
+
+            content.erase(content.begin());
+			content.erase(content.end()-1);
+
             status = insertToHash(hashTable, content, colisao, tam);//chama a funcao de inserção
+
+            if (status)
+                cout << " SUCCESS";
+            else
+                cout << " FAIL";
+
             qtd++; //adiciona mais um na quantidade
         
             fator = (double) qtd / tam; //calcula o fator de carga
@@ -104,17 +110,37 @@ int main(int argc, char **argv) {
             }
         
         } else if (tag == "DELETE") {
-            cout << "DELETE" << endl;
-            removeFromHash(hashTable, content, colisao, tam);
+            cout << "DELETE " << content<< " ";
+
+            content.erase(content.begin());
+			content.erase(content.end()-1);
+
+            status=removeFromHash(hashTable, content, colisao, tam);
+
+            if (status)
+                cout << " SUCCESS";
+            else
+                cout << " FAIL";
+
         } else if (tag == "GET") {
-            cout << "GET" << endl;
-            getFromHash(hashTable, content, colisao, tam);
+            cout << "GET " << content << " ";
+
+            content.erase(content.begin());
+			content.erase(content.end()-1);
+
+            status=getFromHash(hashTable, content, colisao, tam);
+
+            if (status)
+                cout << " SUCCESS";
+            else
+                cout << " FAIL";
         } else
-            cout << "ERROR, UNDEFINED TAG" << endl;
+            cout << "ERROR, UNDEFINED TAG ";
 
         //Karine aqui: não entendi essa parte. Perguntar para o Kristofer
         cout << endl;
         cin >> tag >> content;// le de novo as novas informações
+        content=checkString(content);
     }
 
     //string teste= "umaString";
@@ -135,7 +161,7 @@ node *createHash(int tam) {
 }
 
 bool insertToHash(node *hash, string content, int colisao, int tam) {
-    cout << "\n===INSERCAO===\n" << "Conteudo: " << content << endl;
+    //cout << "\n===INSERCAO===\n" << "Conteudo: " << content << endl;
     //cout<<content<<endl;
     int key, pos;
     //cout<<"content da hash->"<<(hash+pos)->c<<endl;
@@ -145,11 +171,13 @@ bool insertToHash(node *hash, string content, int colisao, int tam) {
     //newNode->c=content;//.copy(content, content.length(), 0);
     //newNode->c=content;
     key = stringXor(content); //retorna onde deve ser inserido na hash
+    cout<<key;
     pos = hash1(key, tam); //faz a função k mod m
+    cout<< " "<<pos;
     //node *auxNode=(hash+pos);
     //cout<<"POSICOES "<<(hash+pos)->c<<" "<<auxNode<<endl;
     //sem colisão, insere normal da hash
-    cout << "Posicao: " << pos << "\nKey: " << key << endl;
+    //out << "Posicao: " << pos << "\nKey: " << key << endl;
 
     //caso a hash esteja vazia ele só poe na posição e abraços
     if (hash[pos].c.empty())
@@ -157,48 +185,39 @@ bool insertToHash(node *hash, string content, int colisao, int tam) {
         //cout<<"content da hash->"<<(hash+pos)->c<<endl;
         hash[pos].c = content;
         hash[pos].deleted=false;
-        cout << "Status: Concluido com sucesso" << endl;
+        cout<< " "<<pos<< " 0";
+        //cout << "Status: Concluido com sucesso" << endl;
 
     }
     else if (hash[pos].c==content)
     {
-    	cout << "Status: Valor ja inserido" << endl;
+    	cout<< " "<<pos<< " 0";
+    	return false;
+    	//cout << "Status: Valor ja inserido" << endl;
     }
     else {
         bool insertedFlag;
-        cout << "Status: Colisao na Insercao" << endl;
+        //cout << "Status: Colisao na Insercao" << endl;
         //ocorreu a colisão, tratara ela com o modo adequado
         switch (colisao) {
             case 0: {
                 insertedFlag = insertEncadeamento(hash, content, pos);
-                if (insertedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return insertedFlag;
                 break;
             }
             case 1: {
                 insertedFlag=insertLinear(hash, content, key, tam);
-                if (insertedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return insertedFlag;
                 break;
             }
             case 2: {
                 insertedFlag=insertQuadratica(hash, content, key, tam);
-                if (insertedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return insertedFlag;
                 break;
             }
             case 3: {
                 insertedFlag = insertHashDuplo(hash, content, key, tam);
-                if (insertedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return insertedFlag;
                 break;
             }
         }
@@ -210,13 +229,17 @@ bool insertToHash(node *hash, string content, int colisao, int tam) {
 
 bool getFromHash(node *hash, string content, int colisao, int tam) {
     //SÓ PESQUISA NA HASH E RETORNA SE ACHOU OU NÃO (até o momento)
-    cout << "\n===BUSCA===\n" << "Conteudo: " << content << endl;
+    //cout << "\n===BUSCA===\n" << "Conteudo: " << content << endl;
     int key, pos;
     key = stringXor(content); //retorna onde deve ser inserido na hash
+    cout<<key;
     pos = hash1(key, tam); //faz a função k mod m
-    cout << "Posicao: " << pos << "\nKey: " << key << endl;
+    cout<< " "<<pos;
+    //out << "Posicao: " << pos << "\nKey: " << key << endl;
     if ((hash + pos)->c == content) {
-        cout << "Status: Concluido com sucesso" << endl;
+        //cout << "Status: Concluido com sucesso" << endl;
+        cout<< " "<<pos<< " 0";
+        
         return true;
     } else {
         bool foundedFlag;
@@ -224,34 +247,22 @@ bool getFromHash(node *hash, string content, int colisao, int tam) {
         switch (colisao) {
             case 0: {
                 foundedFlag = getEncadeamento(hash, content, pos);
-                if (foundedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return foundedFlag;
                 break;
             }
             case 1: {
                 foundedFlag=getLinear(hash, content, key, tam);
-                if (foundedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return foundedFlag;
                 break;
             }
             case 2: {
                 foundedFlag=getQuadratica(hash, content, key, tam);
-                if (foundedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+               	return foundedFlag;
                 break;
             }
             case 3: {
                 foundedFlag=getHashDuplo(hash, content, key, tam);
-                if (foundedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return foundedFlag;
                 break;
             }
         }
@@ -261,15 +272,18 @@ bool getFromHash(node *hash, string content, int colisao, int tam) {
 
 bool removeFromHash(node *hash, string content, int colisao, int tam) {
     //SÓ REMOVE DA HASH NORMALMENTE (até o momento)
-    cout << "\n===REMOCAO===\n" << "Conteudo: " << content << endl;
+    //cout << "\n===REMOCAO===\n" << "Conteudo: " << content << endl;
     int key, pos;
     key = stringXor(content); //retorna onde deve ser inserido na hash
+    cout<<key;
     pos = hash1(key, tam); //faz a função k mod m
-    cout << "Posicao: " << pos << "\nKey: " << key << endl;
+    cout<<" "<<pos;
+    //cout << "Posicao: " << pos << "\nKey: " << key << endl;
 
     //se
     if ((hash + pos)->c == content && (hash + pos)->prox==NULL) {
-        cout << "Status: Concluido com sucesso" << endl;
+        //cout << "Status: Concluido com sucesso" << endl;
+        cout<< " "<<pos<< " 0";
         (hash + pos)->c.clear();
         (hash + pos)->deleted=true;
         return true;
@@ -278,34 +292,22 @@ bool removeFromHash(node *hash, string content, int colisao, int tam) {
         switch (colisao) {
             case 0: {
                 deletedFlag = deleteEncadeamento(hash, content, pos);
-                if (deletedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return deletedFlag;
                 break;
             }
             case 1: {
                 deletedFlag=deleteLinear(hash, content, key, tam);
-                if (deletedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return deletedFlag;
                 break;
             }
             case 2: {
                 deletedFlag=deleteQuadratica(hash, content, key, tam);
-                if (deletedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return deletedFlag;
                 break;
             }
             case 3: {
                 deletedFlag=deleteHashDuplo(hash, content, key, tam);
-                if (deletedFlag)
-                    cout << "\nSUCCESS\n";
-                else
-                    cout << "\nFAIL\n";
+                return deletedFlag;
                 break;
             }
         }
@@ -360,47 +362,74 @@ int stringXor(string content) {
 }
 
 bool insertEncadeamento(node *hash, string content, int pos) {
-    cout << "Insere Encadeado";
+    //cout << "Insere Encadeado";
+    int colisoes=0;
 
     node *auxPointer = (hash + pos);
     while (auxPointer->prox != NULL)//while pra ir até o final da lista encadeada
     {
+    	colisoes++;
         if (auxPointer->c == content)
+        {
+        	cout<< " "<< pos;
+        	cout<< " "<<colisoes;
             return false; //se ja achar o elemento na tabela retorna falso
+        }
 
         auxPointer = auxPointer->prox;
     }
     if (auxPointer->c == content)
+    {
+    	cout<< " "<< pos;
+    	cout<< " "<<colisoes;
         return false;
+    }
 
-
+    colisoes++;
     //no while ele ja foi até o final da lista, agora ele só adiciona na lista encadeada
     node *newNode = new node;
     newNode->c = content;
     newNode->prox = NULL;
     auxPointer->prox = newNode;
 
+    cout<< " "<< pos;
+    cout<< " "<<colisoes;
     return true;
 }
 
 bool getEncadeamento(node *hash, string content, int pos) {
     node *auxPointer = (hash + pos);
-    while (auxPointer != NULL)//while pra ir até o final da lista encadeada
+    int colisoes=0;
+
+    if(auxPointer->prox==NULL)
+    {
+    	cout<< " "<< pos;
+        cout<< " "<<colisoes;
+    	return false;
+    }
+    while (auxPointer!= NULL)//while pra ir até o final da lista encadeada
     {
         //cout<<auxPointer->c;
 
         if (auxPointer->c == content)//if caso ache o nodo com o conteudo que quero
         {
+        	cout<< " "<< pos;
+        	cout<< " "<<colisoes;
             return true;
         }
+        colisoes++;
         auxPointer = auxPointer->prox;
 
     }
+
+    cout<< " "<< pos;
+    cout<< " "<<colisoes;
     return false;
 }
 
 bool deleteEncadeamento(node *hash, string content, int pos) {
     node *auxPointer = (hash + pos);
+    int colisoes=0;
     if(auxPointer->c == content) //Se a primeira posição é a ser deletada ele só atualiza o valor e deleta o prox
     {
     	node *toBeDeleted =auxPointer->prox;
@@ -409,6 +438,8 @@ bool deleteEncadeamento(node *hash, string content, int pos) {
     	auxPointer->prox = auxPointer->prox->prox;
     	free(toBeDeleted);
 
+    	cout<< " "<< pos;
+    	cout<< " "<<colisoes;
     	return true;
     }
     while (auxPointer==NULL)//while pra ir até o final da lista encadeada
@@ -423,16 +454,21 @@ bool deleteEncadeamento(node *hash, string content, int pos) {
     		auxPointer->prox = auxPointer->prox->prox;
     		free(toBeDeleted);
 
+    		cout<< " "<< pos;
+    		cout<< " "<<colisoes;
             return true;
         }
         auxPointer = auxPointer->prox;
 
     }
+    cout<< " "<< pos;
+    cout<< " "<<colisoes;
     return false;
 }
 
 bool insertLinear(node *hash, string content, int key, int tam) {
-    cout << "Insere Linear";
+    //cout << "Insere Linear";
+    
     int pos;
 
     for (int i = 0; i < tam; i++)
@@ -442,10 +478,12 @@ bool insertLinear(node *hash, string content, int key, int tam) {
         {
             hash[pos].c = content;
             hash[pos].deleted=false;
+            cout<< " "<<pos <<" "<< i;
             return true;
         }
         else if(hash[pos].c == content)
         {
+        	cout<< " "<<pos <<" "<< i;
         	return false;
         }
 
@@ -454,7 +492,7 @@ bool insertLinear(node *hash, string content, int key, int tam) {
 
 bool getLinear(node *hash, string content, int key, int tam)
 {
-    cout << "get linear";
+    //cout << "get linear";
     int pos;
 
     for (int i = 0; i < tam; i++)
@@ -462,9 +500,12 @@ bool getLinear(node *hash, string content, int key, int tam)
     	pos=hash1(key + i, tam);
         if(hash[pos].c == content)
         {
+        	cout<< " "<<pos <<" "<< i;
         	return true;
         }
-        else if (hash[pos].c.empty() && hash[pos].deleted==false){
+        else if (hash[pos].c.empty() && hash[pos].deleted==false)
+        {
+        	cout<< " "<<pos <<" "<< i;
             return false;
         }
     }
@@ -472,7 +513,8 @@ bool getLinear(node *hash, string content, int key, int tam)
 
 bool deleteLinear(node *hash, string content, int key, int tam)
 {
-    cout << "delete linear";
+    //cout << "delete linear";
+    //int colisoes=0;
     int pos;
 
     for (int i = 0; i < tam; i++)
@@ -482,9 +524,12 @@ bool deleteLinear(node *hash, string content, int key, int tam)
         {
             hash[pos].c.clear();
             hash[pos].deleted=true;
+            cout<< " "<<pos <<" "<< i;
         	return true;
         }
-        else if (hash[pos].c.empty() && hash[pos].deleted==false){
+        else if (hash[pos].c.empty() && hash[pos].deleted==false)
+        {
+        	cout<< " "<<pos <<" "<< i;
             return false;
         }
     
@@ -494,7 +539,7 @@ bool deleteLinear(node *hash, string content, int key, int tam)
 
 bool insertQuadratica(node *hash, string content, int key, int tam) {
     //Verificar essa função
-    cout << "Insere Quadratico";
+    //cout << "Insere Quadratico";
     int pos = 0;
 
     for (int i = 1; pos < tam; i++){
@@ -503,9 +548,12 @@ bool insertQuadratica(node *hash, string content, int key, int tam) {
         {
             hash[pos].c = content;
             hash[pos].deleted = false;
+            cout<< " "<<pos <<" "<< i;
             return true;
         }
-        else if(hash[pos].c == content){
+        else if(hash[pos].c == content)
+        {
+        	cout<< " "<<pos <<" "<< i;
             return false;
         }
     }
@@ -513,7 +561,7 @@ bool insertQuadratica(node *hash, string content, int key, int tam) {
 }
 
 bool getQuadratica(node *hash, string content, int key, int tam){
-    cout<<"get quadratica";
+    //cout<<"get quadratica";
     int pos;
 
     for (int i = 0; i < tam; i++)
@@ -521,15 +569,18 @@ bool getQuadratica(node *hash, string content, int key, int tam){
         pos=hash1(key + i + i*i, tam);
         if(hash[pos].c == content)
         {
+        	cout<< " "<<pos <<" "<< i;
             return true;
         }
-        else if (hash[pos].c.empty() && hash[pos].deleted==false){
+        else if (hash[pos].c.empty() && hash[pos].deleted==false)
+        {
+        	cout<< " "<<pos <<" "<< i;
             return false;
         }
     }
 }
 bool deleteQuadratica(node *hash, string content, int key, int tam){
-    cout<<"get quadratica";
+    //cout<<"get quadratica";
 
     int pos;
 
@@ -540,9 +591,12 @@ bool deleteQuadratica(node *hash, string content, int key, int tam){
         {
             hash[pos].c.clear();
             hash[pos].deleted=true;
+            cout<< " "<<pos <<" "<< i;
             return true;
         }
-        else if (hash[pos].c.empty() && hash[pos].deleted==false){
+        else if (hash[pos].c.empty() && hash[pos].deleted==false)
+        {
+        	cout<< " "<<pos <<" "<< i;
             return false;
         }
     
@@ -550,7 +604,7 @@ bool deleteQuadratica(node *hash, string content, int key, int tam){
 }
 
 bool insertHashDuplo(node *hash, string content, int key, int tam) {
-    cout << "Insere Hash Duplo";
+    //cout << "Insere Hash Duplo";
     int aux=0, h1, h2;
     h1 = hash1(key, tam);
     h2 = hash2(key, tam);
@@ -560,9 +614,11 @@ bool insertHashDuplo(node *hash, string content, int key, int tam) {
         if(hash[aux].c.empty()){
             hash[aux].c = content;
             hash[aux].deleted=false;
+            cout<< " "<<aux <<" "<< i;
             return true;
         }
         else if(hash[aux].c == content){
+        	cout<< " "<<aux <<" "<< i;
             return false;
         }
     }
@@ -579,10 +635,12 @@ bool deleteHashDuplo(node *hash, string content, int key, int tam)
         if(hash[aux].c == content){
             hash[aux].c.clear();
             hash[aux].deleted=true;
+            cout<< " "<<aux <<" "<< i;
             return true;
         }
         else if (hash[aux].c.empty() && (hash[aux].deleted==false))
         {
+        	cout<< " "<<aux <<" "<< i;
             return false;
         }
     }
@@ -598,10 +656,12 @@ bool getHashDuplo(node *hash, string content, int key, int tam)
         aux = hash1((h1 + i * h2), tam);
         if (hash[aux].c == content)
         {
+        	cout<< " "<<aux <<" "<< i;
             return true;
         }
         else if (hash[aux].c.empty() && (hash[aux].deleted==false))
         {
+        	cout<< " "<<aux <<" "<< i;
             return false;
         }
     }
@@ -642,4 +702,15 @@ int selectTreatment(char **argv)
         return 4;
     } else
         return -1;
+}
+
+string checkString(string content)
+{
+	while(content.length()>101)// tem q ser 101 pq tem as aspas
+	{
+  	  	cout<<"ERROR"<<endl;
+    	cin>>content;
+	} 	
+	
+	return content;
 }
